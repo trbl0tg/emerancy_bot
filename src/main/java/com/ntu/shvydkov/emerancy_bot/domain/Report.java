cb381849fdb5b48ef632bb511b48eeb23d466c90
Gleb Shvydkov <gleb.shvydkov@syncier.com>
2022-12-05 23:33:14 +0200

    added report state localized message

--- src/main/java/com/ntu/shvydkov/emerancy_bot/domain/Report.java
+++ src/main/java/com/ntu/shvydkov/emerancy_bot/domain/Report.java
@@ -38,12 +38,30 @@ public class Report {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
         String s = "Назва : " + this.title + "\n" +
                 "Рівень загрози : " + DangerLevelUtils.dangerLevelToText(this.getDangerLevel()) + "\n" +
-                "Дата : " + this.getCreated().format(formatter) + "\n";
+                "Дата : " + this.getCreated().format(formatter)  +
+                "\nCтатус : " + getDisplayMessage(this.getReportState()) +
+                "\n";
         if (this.textLocation != null) {
             s += "Локація : " + this.textLocation + "\n";
         } else {
             s += "Локація : ";
         }
         return s;
+    }
+
+    private String getDisplayMessage(ReportState reportState) {
+        switch (reportState){
+            case DRAFT:
+                return "В черзі на розгляд.";
+            case APPROVED:
+                return "Корректність підтверджено.";
+            case PROCESSED:
+                return "Розглянуто.";
+            case ACCEPTED_BY_AUTHORITY:
+                return "Опрацьовано службами.";
+            case REJECTED:
+                return "Відмовлено у опрацюванні.";
+        }
+        return "Не відомий статус публікації!";
     }
 }
